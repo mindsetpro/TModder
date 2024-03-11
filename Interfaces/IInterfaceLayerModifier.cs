a2d5a9daad54f018f27488cd4be255f9d4245a42
Assasian.Nator <138173273+mindsetpro@users.noreply.github.com>
2024-03-10 20:34:45 -0400

    Update IInterfaceLayerModifier.cs

--- Interfaces/IInterfaceLayerModifier.cs
+++ Interfaces/IInterfaceLayerModifier.cs
@@ -1,9 +1,10 @@
+using Microsoft.Xna.Framework.Graphics;
 using System.Collections.Generic;
 
 namespace TModder.Interfaces
 {
     public interface IInterfaceLayerModifier
     {
-        void ModifyInterfaceLayers(List<MyInterfaceLayer> layers);
+        void ModifyInterfaceLayers(List<GameInterfaceLayer> layers);
     }
 }
