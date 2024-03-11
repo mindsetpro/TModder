e5f109297ed42bcd358c5e6bafce34cce701c68d
Assasian.Nator <138173273+mindsetpro@users.noreply.github.com>
2024-03-10 20:31:05 -0400

    Create IInterfaceLayerModifier.cs

---
+++ Interfaces/IInterfaceLayerModifier.cs
@@ -0,0 +1,9 @@
+using System.Collections.Generic;
+
+namespace TModder.Interfaces
+{
+    public interface IInterfaceLayerModifier
+    {
+        void ModifyInterfaceLayers(List<MyInterfaceLayer> layers);
+    }
+}
